f32cd90ae3a8d241b4788b776674afa5b995a306
nguyenvanbinh981988 <binhnguyenvan981988@gmail.com>
2022-07-14 16:12:41 +0700

    new

---
+++ SpringCotroller_TH_2/src/main/java/com/example/springcotroller_th_2/ApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.example.springcotroller_th_2;
+
+import com.example.springcotroller_th_2.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
